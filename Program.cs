bdf8d9a58454b5a03aa4769b979297499ae3961a
Lucas Ramos <lucasramos.lr260@gmail.com>
2022-10-21 22:16:10 -0300

    Adiciona a solução do exercício

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Fluxograma da Engenharia\n");
+Console.Write("O objeto está se movendo (S/N)? ");
+bool EstaSeMovendo = Console.ReadLine() ! .Trim() .Substring(0, 1) .ToUpper() == "S"; 
+Console.Write("Deveria? ");
+bool Deveria = Console.ReadLine() ! .Trim() .Substring(0, 1) .ToUpper() == "S";
+string mensagem;
+if (!EstaSeMovendo)
+{
+    if (!Deveria)
+    {
+        mensagem = "Ótimo!";
+    }
+    else
+    {
+        mensagem = "Use WD-40.";
+    }
+}
+else
+{
+    if (Deveria)
+    {
+        mensagem = "Ótimo!";
+    }
+    else
+    {
+        mensagem = "Use silver tape.";
+    }
+}
+Console.WriteLine();
+Console.WriteLine(mensagem);
